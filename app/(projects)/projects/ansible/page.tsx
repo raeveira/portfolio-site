40b4fcbe34058dd07269554569c84730a906368b
rae <rae@raeveira.nl>
2024-11-02 15:34:43 +0100

    enhancement: updated projects page
    Signed-off-by: rae <rae@raeveira.nl>

--- app/(projects)/projects/ansible/page.tsx
+++ app/(projects)/projects/ansible/page.tsx
@@ -1,7 +1,19 @@
+'use client';
+import React from 'react';
+import ProjectStructure from "@/components/project/ProjectStructure";
+import ProjectSectionBanner from "@/components/project/ProjectSectionBanner";
+import ProjectOverview from "@/components/project/ProjectOverview";
+import ProjectHighlights from "@/components/project/ProjectHighlights";
+
 export default function AnsibleProjectPage() {
     return (
-        <main className="min-h-dvh">
-
-        </main>
-    )
-};
+        <>
+            <ProjectStructure ItemIdentifier={1}/>
+            <ProjectSectionBanner ItemIdentifier={1}/>
+            <main className="min-h-dvh">
+                <ProjectOverview ItemIdentifier={1}/>
+                <ProjectHighlights ItemIdentifier={1}/>
+            </main>
+        </>
+    );
+}

47e8c642cfbc4297a4d493d43b3044611e2c3c98
rae <rae@raeveira.nl>
2024-11-30 16:04:16 +0100

    // TODO create notfound & projectSkeleton components
    Signed-off-by: rae <rae@raeveira.nl>

---
+++ app/(general)/info/page.tsx
@@ -0,0 +1,10 @@
+const InfoPage = () => {
+    return (
+        <div>
+            <h1>Info Page</h1>
+            <p>This is the info page</p>
+        </div>
+    )
+}
+
+export default InfoPage;

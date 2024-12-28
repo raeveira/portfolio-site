4e98b5b8662a99456ad7df8efc06f5eaddf150ff
rae <rae@raeveira.nl>
2024-12-28 11:44:36 +0100

    Revert "removed unused heroportrait"
    This reverts commit fb8a51fd8c882f4702c482be65c2d8e6b0e3996a.

--- app/(general)/page.tsx
+++ app/(general)/page.tsx
@@ -1,4 +1,5 @@
 import Hero from '@/components/Hero';
+import HeroPortrait from '@/components/HeroPortrait';
 import ProjectSection from "@/components/project/ProjectSection";
 
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <main className="min-h-dvh">
       <Hero />
+      <HeroPortrait />
       <ProjectSection />
     </main>
   );

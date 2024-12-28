3a84bb3dceed7b3ea18c79e472ae458aae516812
rae <rae@raeveira.nl>
2024-12-28 12:00:12 +0100

    added my real experience
    Signed-off-by: rae <rae@raeveira.nl>

--- data/experiences.ts
+++ data/experiences.ts
@@ -1,15 +1,9 @@
 const experiences = [
     {
-        title: 'Discord',
-        subheader: 'Senior Product Designer, Core',
-        date: '07/\'23 - Present',
-        content: 'I\'m designing the future of Discord\'s messaging & core product experiences, amongst other top secret projects.'
-    },
-    {
-        title: 'Google',
-        subheader: 'Product Designer, Core',
-        date: '07/\'23 - Present',
-        content: 'I\'m designing the future of Discord\'s messaging & core product experiences, amongst other top secret projects.'
+        title: 'Fctry B.V.',
+        subheader: 'Internship, Software Developer',
+        date: '02/\'24 - 07\'24',
+        content: 'I worked on core elements for the company as such a customer portal and some other internal tools.'
     }
 ]
 

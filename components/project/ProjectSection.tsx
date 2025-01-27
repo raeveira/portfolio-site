96cba93ae23973751eab39fbea99b3c4334ec9ac
raeveira <rae@raeveira.nl>
2025-01-27 19:38:51 +0100

    moved images to better folder structure, added better no project handling, project loading state and project error state

--- components/project/ProjectSection.tsx
+++ components/project/ProjectSection.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, {useEffect, useState} from 'react';
 import ProjectCard from '@/components/project/ProjectCard';
-import { fetchData } from '@/app/actions/fetchAllData';
+import {fetchData} from '@/app/actions/fetchAllData';
 import {Project} from '@prisma/client';
 
 export default function ProjectSection() {
@@ -11,25 +11,43 @@ export default function ProjectSection() {
     useEffect(() => {
         const fetchProjects = async () => {
             const data = await fetchData();
-            if(data) setProject(data);
+            if (data) setProject(data);
         }
 
         fetchProjects().then(() => setLoading(false));
     }, []);
 
     if (loading) {
-        return <div>Project Loading</div>
+        return (
+            <div className="flex justify-center items-center h-64">
+                <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+            </div>
+        )
     }
 
     if (!projects) {
-        return <div>Project not found</div>;
+        return (
+            <div className="flex justify-center items-center h-64">
+                <p className="text-xl text-gray-600">Error loading projects. Please try again later.</p>
+            </div>
+        )
+    }
+
+    if (projects.length === 0) {
+        return (
+            <div className="flex flex-col justify-center items-center h-64 space-y-4">
+                <p className="text-xl text-gray-600">No projects found.</p>
+            </div>
+        )
     }
 
     return (
-        <section className={'relative flex pb-[200px] pt-6 items-center flex-col gap-y-20 1920px:pt-16 max-991px:items-stretch max-991px:px-12 max-767px:px-6 max-767px:pt-12 max-479px:gap-y-10 max-479px:pt-0 max-479px:px-4 max-479px:pb-20'}>
-            <div className={'relative flex w-[882px] items-stretch flex-col gap-y-10 1920px:gap-y-20 1920px:w-[1440px] 1440px:w-[1044px] max-991px:w-auto max-479px:gap-y-10'}>
+        <section
+            className={'relative flex pb-[200px] pt-6 items-center flex-col gap-y-20 1920px:pt-16 max-991px:items-stretch max-991px:px-12 max-767px:px-6 max-767px:pt-12 max-479px:gap-y-10 max-479px:pt-0 max-479px:px-4 max-479px:pb-20'}>
+            <div
+                className={'relative flex w-[882px] items-stretch flex-col gap-y-10 1920px:gap-y-20 1920px:w-[1440px] 1440px:w-[1044px] max-991px:w-auto max-479px:gap-y-10'}>
                 {projects && projects.map((item, index) => (
-                    <ProjectCard key={index} ProjectID={item.id} />
+                    <ProjectCard key={index} ProjectID={item.id}/>
                 ))}
             </div>
         </section>
--- data/aboutMeData.ts
+++ data/aboutMeData.ts
@@ -1,7 +1,7 @@
 const aboutMeData = [
     {
-        src: '/logos/logo.png',
-        src1: '/logos/logo.png',
+        src: '/infoPage/0fc1441f14ebf3529ef93bc424750499.png',
+        src1: '/infoPage/3be255f23b596f31ff26d7e4d770dd98.png',
         text3: 'In August of 2022, I entered the world of Software Development and fell in love with the idea of creating digital products that could be used by millions of people.',
         text4: 'I started my journey by learning the basics of HTML, CSS, and JavaScript, and then moved on to more advanced topics like React, Node.js, and PostgreSQL.',
         text5: 'This is my story — alongside some flicks from myself and my journey.',
@@ -10,8 +10,8 @@ const aboutMeData = [
         text2: 'To my advantage, I was able to leverage a lot of the skills and principles I had picked up in earlier years to greatly expedite my journey of self-learning Software Development.'
     },
     {
-        src: '/logos/logo.png',
-        src1: '/logos/logo.png',
+        src: '/infoPage/b2cf3dd9d355a6ccf7f66b4ff2d31b71.png',
+        src1: '/infoPage/b3f648fb71cc2aa0cf5669c4fa98860f.png',
         text3: 'When I was finished with my internship, I took the opportunity to learn more about the world of Software Development and so I started learning the basics of Java.',
         text4: 'Long story short, my current goal is to become a Full Stack Java Developer.',
         text5: 'But I wanted more.',
@@ -20,8 +20,8 @@ const aboutMeData = [
         text2: 'So I threw myself into the deep end and started learning Java from the inside out. I wanted to understand how the language worked and how I could use it to build amazing things.'
     },
     {
-        src: '/logos/logo.png',
-        src1: '/logos/logo.png',
+        src: '/infoPage/b1520e2dfd51cd3c754d20c4269f8d6f.png',
+        src1: '/infoPage/h5sn2dqcwktz.png',
         text3: 'To my advantage, I learned the basics of Java quite quickly and was able to build a few simple applications in a matter of weeks.',
         text4: 'So I started using Java to build APIs in school. I was able to test my skills and build a few simple APIs.',
         text5: 'But I wanted more so I started learning the basics of Spring Boot.',

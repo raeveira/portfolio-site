fb17ca3742ef7aad135acad241bed2742454f101
rae <rae@raeveira.nl>
2025-02-01 19:27:09 +0100

    added google analytics

--- app/(projects)/layout.tsx
+++ app/(projects)/layout.tsx
@@ -1,27 +1,14 @@
 'use client';
-import "../globals.css";
-import {Inter} from 'next/font/google';
 import Footer from "@/components/Footer";
 import ProjectHeader from '@/components/project/ProjectHeader';
-
-const inter = Inter({
-    style: 'normal',
-    weight: ['400', '700'],
-    display: 'swap',
-    adjustFontFallback: true,
-    fallback: ['Arial', 'sans-serif'],
-    preload: true,
-    subsets: ['latin'],
-});
+import React from "react";
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
-        <html lang="en">
-        <body className={inter.className + ' bg-background text-onBackground flex-col items-center'}>
-        <ProjectHeader/>
-        {children}
-        <Footer/>
-        </body>
-        </html>
+        <>
+            <ProjectHeader/>
+            {children}
+            <Footer/>
+        </>
     );
 }
---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import "../globals.css";
+import {Inter} from 'next/font/google';
+import { GoogleAnalytics } from '@next/third-parties/google'
+import React from "react";
+
+const inter = Inter({
+    style: 'normal',
+    weight: ['400', '700'],
+    display: 'swap',
+    adjustFontFallback: true,
+    fallback: ['Arial', 'sans-serif'],
+    preload: true,
+    subsets: ['latin'],
+});
+
+export default function RootLayout({children}: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+        <body className={inter.className + ' bg-background text-onBackground flex-col items-center'}>
+        {children}
+        </body>
+        <GoogleAnalytics gaId="G-LBCZB8QVT8" />
+        </html>
+    );
+}
